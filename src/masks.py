afdfa8d1489b0e3a742525fae9d293f98ee22dea
egor tsvetkov <bye.dzinushi@gmail.com>
2026-04-19 15:02:23 +0200

    переделал код проверки ввода

--- src/masks.py
+++ src/masks.py
@@ -1,6 +1,19 @@
 def get_mask_card_number(card_number: str) -> str:
     """Функция приема номера карты пользователя"""
+    # проверка ввода на "" или None
+    if not card_number:
+        return "Введен некорректный номер карты"
+    # проверка кода на наличие "*", "-", "&" итд.
+    if not all(char.isdigit() or char.isspace() for char in card_number):
+        return "Введен некорректный номер карты"
+    # удаление пробелов
+    card_number = card_number.replace(" ", "")
+    # проверка ввода на минимальную длину строки
+    if len(card_number) < 10:
+        return "Введен слишком короткий номер карты"
+
     edit_card_number = ""
+
     for i in range(len(card_number)):
         if 6 <= i < len(card_number) - 4:
             edit_card_number += "*"
